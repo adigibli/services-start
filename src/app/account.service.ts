bd50fd104febc17e45de46164bda034eefd67b1a
adigibli <gibliadi@gmail.com>
2020-10-10 22:58:42 +0300

    first commit

---
+++ src/app/account.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, EventEmitter } from "@angular/core";
+
+import { LoggingService } from "./logging.service";
+
+@Injectable()
+export class AccountService {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  statusUpdated = new EventEmitter<string>();
+
+  constructor(private loggingService: LoggingService){}
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({name, status});
+    this.loggingService.logStatusChanged(status);
+  }
+
+  changeStatus(id: number, newStatus: string) {
+    this.accounts[id].status = newStatus;
+    this.loggingService.logStatusChanged(newStatus);
+  } 
+
+}
---
+++ src/app/logging.service.ts
@@ -0,0 +1,9 @@
+
+
+
+export class LoggingService {
+  logStatusChanged(newStatus: string){
+    console.log('A server status changed, new status: ' + newStatus);
+  }
+
+}
